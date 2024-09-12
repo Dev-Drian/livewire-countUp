1e9846f00110ac9c94066750989f44a8d4117887
Adecas-Eg <matiose12@gmail.com>
2024-09-12 13:51:27 -0500

    mejora vistas

--- src/AnimatedCounterServiceProvider.php
+++ src/AnimatedCounterServiceProvider.php
@@ -4,15 +4,23 @@ namespace AdrianDev\AnimatedCounter;
 
 use Illuminate\Support\ServiceProvider;
 use Livewire\Livewire;
-// use adrian_dev\AnimatedCounter\Http\Livewire\AnimatedCounter;
 use AdrianDev\AnimatedCounter\Http\Livewire\AnimatedCounter as LivewireAnimatedCounter;
 
 class AnimatedCounterServiceProvider extends ServiceProvider
 {
+    public function register()
+    {
+        //
+    }
+
     public function boot()
     {
-        $this->loadViewsFrom(__DIR__.'/resources/views', 'animated-counter');
-        
+        $this->loadViewsFrom(__DIR__ . '/resources/views', 'animated-counter');
+
+        $this->publishes([
+            __DIR__ . '/resources/views' => resource_path('views/vendor/animated-counter'),
+        ], 'animated-counter-views');
+
         Livewire::component('animated-counter', LivewireAnimatedCounter::class);
     }
 }
